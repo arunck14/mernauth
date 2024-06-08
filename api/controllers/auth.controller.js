8cb0fe07df8102dc64b66424b6531acc812e40e4
arunck14 <arun14.1998@gmail.com>
2024-06-08 23:25:02 +0530

    added middleware to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs' ;
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res) =>{
+export const signup = async (req,res,next) =>{
     // res.json({
     //     message: " api called successfully from auth controller"
     // });
@@ -15,7 +16,9 @@ export const signup = async (req,res) =>{
    res.status(201).json({message : "user created successfully"});
     }
     catch(error){
-        res.status(500).json(error.message);
+        // res.status(500).json(error.message); 
+        // next(errorHandler(300,"something went wrong"));  
+        next(error);
     }
   
 };
