8990045ba8e751bb94313aef5e890aeeeff213b4
arunck14 <arun14.1998@gmail.com>
2024-06-08 19:54:01 +0530

    created user model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import mongoose, { mongo } from 'mongoose';
+// import dotenv from 'dotenv';
+// dotenv.config;
+const MONGO="mongodb+srv://arunmernauth:arunmernauth@mern-auth.lp4wfce.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=mern-auth"
+
 const  app = express();
+mongoose.connect(MONGO);
 
 app.listen(3000,()=>{
+    // console.log(process.env.MONGO);
     console.log("server listening port 3000");
+    
 })
