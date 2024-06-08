0d6741d7af24a91a786af5cb8326293657f604d4
arunck14 <arun14.1998@gmail.com>
2024-06-08 21:58:57 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose, { mongo } from 'mongoose';
+import userRoutes  from  './routes/user.route.js';
 // import dotenv from 'dotenv';
 // dotenv.config;
 const MONGO="mongodb+srv://arunmernauth:arunmernauth@mern-auth.lp4wfce.mongodb.net/mern-auth?retryWrites=true&w=majority&appName=mern-auth"
@@ -7,6 +8,7 @@ const MONGO="mongodb+srv://arunmernauth:arunmernauth@mern-auth.lp4wfce.mongodb.n
 const  app = express();
 mongoose.connect(MONGO);
 
+app.use ("/api/user",userRoutes);
 app.listen(3000,()=>{
     // console.log(process.env.MONGO);
     console.log("server listening port 3000");
