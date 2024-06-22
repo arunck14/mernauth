5593f09dd5a1bc02dc6104b8c7c04d6a7d8536f1
arunck14 <arun14.1998@gmail.com>
2024-06-22 23:03:03 +0530

    created signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs' ;
 import { errorHandler } from "../utils/error.js";
-
+import jwt  from "jsonwebtoken";
+const JWT_SC = 'authcontabcdegfsfdsldkfjsld';
 export const signup = async (req,res,next) =>{
     // res.json({
     //     message: " api called successfully from auth controller"
@@ -20,5 +21,27 @@ export const signup = async (req,res,next) =>{
         // next(errorHandler(300,"something went wrong"));  
         next(error);
     }
+  
+};
+export const signin = async (req,res,next) =>{
+    // res.json({
+    //     message: " api called successfully from auth controller"
+    // });
+
+   const {email,password} =req.body;
+   try {
+    const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(401,'User not found'));
+    const validPassword  = bcryptjs.compareSync(password,validUser.password);
+    if(!validPassword ) return next(errorHandler(401 , 'wrong credentials'));
+    const token = jwt.sign({id:validUser._id},JWT_SC); //for saftery ,use in proccess.inv
+   
+    const {password:hashedPassword, ...rest} =validUser._doc;  //add _doc to distructure properly,this step is used to remove password in json when api is called
+    const expiryDate = new Date(Date.now()+360000) ; //1 hour expiry for cookie
+
+    res.cookie('access_token',token, {httpOnly:true,expires:expiryDate}).status(200).json(rest); //to set cookie; rest is passed for saftey to remove password
+   } catch (error) {
+    next(error);
+   }
   
 };
